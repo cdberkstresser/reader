04a27ed29694ba85e45e8318a0a94d5024037403
Caleb Berkstresser <cdberkstresser@protonmail.com>
2019-10-31 09:43:54 -0500

    Adding reader settings to page

--- popup/setLanguages.js
+++
@@ -1,7 +0,0 @@
-function setLanguages() {
-    var x = document.getElementById("languages");
-    var voices = speechSynthesis.getVoices();
-    for (var n = 0; n < voices.length; ++n) {
-        x.options[x.options.length] = new Option(voices[n].name, n);
-    }
-}
