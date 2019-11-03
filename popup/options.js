4579f361e899aa07ff7179d8b8e9f8eb56b90760
Caleb Berkstresser <cdberkstresser@protonmail.com>
2019-11-03 13:04:24 -0600

    Updating options for experimental mode

--- popup/options.js
+++ popup/options.js
@@ -1,63 +1,25 @@
 function saveOptions(e) {
     browser.storage.sync.set({
-        voice: document.querySelector("#voice").value,
-        speed: document.querySelector("#speed").value,
-        pitch: document.querySelector("#pitch").value
+        experimentalMode: document.querySelector("#experimentalMode").value,
+        indefiniteArticleColor: document.querySelector("#indefiniteArticleColor").value,
+        definiteArticleColor: document.querySelector("#definiteArticleColor").value,
+        dyslexify: document.querySelector("#dyslexify").value
     });
     e.preventDefault();
 }
-
 function restoreOptions() {
-    var storageVoice = browser.storage.managed.get('voice');
-    var storageSpeed = browser.storage.managed.get('speed');
-    var storagePitch = browser.storage.managed.get('pitch');
-
-    var gettingVoice = browser.storage.sync.get('voice');
-    gettingVoice.then((res) => {
-        document.querySelector("#voice").value = res.voice || 0;
-    });
-
-    var gettingSpeed = browser.storage.sync.get('speed');
-    gettingSpeed.then((res) => {
-        document.querySelector("#speed").value = res.speed || 1;
-    });
-    var gettingPitch = browser.storage.sync.get('pitch');
-    gettingPitch.then((res) => {
-        document.querySelector("#pitch").value = res.pitch || 1;
-    });
+	try {
+		var api = chrome || browser;
+		api.storage.local.get(null, res => {
+			document.querySelector("#experimentalMode").value = res.experimentalMode || 0;
+			document.querySelector("#indefiniteArticleColor").value = res.indefiniteArticleColor || 0;
+            document.querySelector("#definiteArticleColor").value = res.definiteArticleColor || 0;
+            document.querySelector("#dyslexify").value = res.dyslexify || 0;
+		});
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector("form").addEventListener("change", saveOptions);
-
-
-/**
- * Options menu for the top of the quiz.
- */
-try {
-    var settingsDiv = document.getElementById("settings");
-    var voiceSelect = document.getElementById("voice");
-    var speedSelect = document.getElementById("speed");
-    var pitchSelect = document.getElementById("pitch");
-
-    //the voices
-    var voices = speechSynthesis.getVoices();
-    for (var n = 0; n < voices.length; ++n) {
-        voiceSelect.options[voiceSelect.options.length] = new Option(voices[n].name, n);
-    }
-
-    //the speeds
-    for (var n = 5; n <= 15; n++) {
-        speedSelect.options[speedSelect.options.length] = new Option((n / 10) + "x", n / 10);
-    }
-    speedSelect.selectedIndex = 5;
-
-    //the pitch
-    for (var n = 0; n <= 20; n++) {
-        pitchSelect.options[pitchSelect.options.length] = new Option((n / 10), n / 10);
-    }
-    pitchSelect.selectedIndex = 10;
-
-} catch (err) {
-
-}
