5fccb2b3a4cb2f57f510f317845358b6cf1700ac
Caleb Berkstresser <cdberkstresser@protonmail.com>
2019-11-05 18:22:42 -0600

    Fixing compression build script for windows

--- build.ps1
+++
@@ -1,6 +0,0 @@
-#exclude git and build files
-$exclude = @("*.git*","build.*")
-#include everything else in this directory
-$files = Get-ChildItem -Path . -Exclude $exclude
-#create a zip file in the parent folder
-Compress-Archive -Path $files -DestinationPath "../reader.zip" -CompressionLevel Fastest -Force
