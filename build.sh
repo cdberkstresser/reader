db4420b3ade5199f7c296fbe645001e5e82db90a
Caleb Berkstresser <cdberkstresser@protonmail.com>
2019-11-11 18:48:35 -0600

    Updating build script and including Adelaide's sync changes

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-zip -r -FS ../reader.zip * --exclude *.git*  --exclude *build.*
+zip -r -FS ../reader-Firefox.zip * --exclude *.git*  --exclude *build.*
+mv manifest.json manifest.base
+jq 'del(.browser_specific_settings)' manifest.base > manifest.json
+zip -r -FS ../reader-Chrome.zip * --exclude *.git*  --exclude *build.* --exclude *manifest.base
+rm manifest.base
