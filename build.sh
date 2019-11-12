9040b9f41678ad8e24fb40e9c1eb7ca91c26eab8
Caleb Berkstresser <cdberkstresser@protonmail.com>
2019-11-11 18:54:42 -0600

    Updating build scripts again

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ zip -r -FS ../reader-Firefox.zip * --exclude *.git*  --exclude *build.*
 mv manifest.json manifest.base
 jq 'del(.browser_specific_settings)' manifest.base > manifest.json
 zip -r -FS ../reader-Chrome.zip * --exclude *.git*  --exclude *build.* --exclude *manifest.base
-rm manifest.base
+rm manifest.json
+mv manifest.base manifest.json
