2274ed04bdfe60a62da85aae6b4b0a1451261a01
Caleb Berkstresser <cdberkstresser@protonmail.com>
2020-05-04 14:19:41 -0500

    Updating build to add firefox specific settings to manifest.json rather than remove them from it.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-zip -r -FS ../reader-Firefox.zip * --exclude *.git*  --exclude *build.*
-mv manifest.json manifest.base
-jq 'del(.browser_specific_settings)' manifest.base > manifest.json
 zip -r -FS ../reader-Chrome.zip * --exclude *.git*  --exclude *build.* --exclude *manifest.base
+mv manifest.json manifest.base
+jq '.browser_specific_settings.gecko += {id : "{3f0845bb-e6dc-4c4c-a43d-5b49221a3b13}"}' manifest.base > manifest.json
+zip -r -FS ../reader-Firefox.zip * --exclude *.git*  --exclude *build.*
 rm manifest.json
 mv manifest.base manifest.json
